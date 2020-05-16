24eb4923cda08d6b8b170e5e58da4de4e741649b
saitha vismith <sasithavismith@gmail.com>
2020-05-16 23:13:48 +0530

    Navigation menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'AdZone') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,18 +18,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('fontawesome/css/all.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="background: rgb(159,25, 50) !important;">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
+                <div class="col-lg-2">
+                    <div class="row">
+                        <a class="navbar-brand" href="{{ url('/') }}" style="color: #fff; font-size: 28px;">AdZone</a>
+                        
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -41,11 +40,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}" style="color: #fff;">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}" style="color: #fff;">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -67,8 +66,52 @@
                                 </div>
                             </li>
                         @endguest
+                        <li style="margin-top: 9px;"><a href="{{url('')}}" style="color: #fff; text-decoration: none;">Add Post</a></li>
                     </ul>
                 </div>
+                    </div>
+                </div>
+                <div class="col-lg-10">
+                    <div class="row">
+                        <div class="col-4">
+                            <form action="" method="POST" class="form-horizontal">
+                                <div class="form-group row">
+                                    <div class="col-8">
+                                        <input type="text" name="searachonproduct" id="searachonproduct" placeholder="Enter Product" class="form-control" style="margin-top: 5px;">
+                                    </div>
+                                    <div class="col-4">
+                                        <input type="submit" class="btn btn-default" name="" value="Search" style="margin-top: 5px;">
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="col-lg-8">
+                            <form action="" method="POST" class="form-horizontal">
+                              
+                                <div class="form-group row">
+                                    <div class="col-6">
+                                        <input type="text" name="states" id="states" class="form-control" placeholder="Enter State" style="margin-top: 5px;">
+                                     
+                                         </div>
+                                    <div class="col-4">
+                                        <select class="form-control dropdown" id="catogories" name="catogories" style="margin-top: 5px;">
+                                            <option>Select</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-lg-2">
+                                        <input type="submit" name="searchads" value="Search" class="btn btn-default" style="margin-top: 5px;">
+                                    </div>
+                                </div>
+                                </form>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
             </div>
         </nav>
 
